30e0be6b29f675c02d60b873525473a2b64b8446
honion46 <mozberk@gmail.com>
2026-08-25 23:12:16 +0300

    Delete src/components directory

--- src/components/DebugSnapshotsViewer.tsx
+++
@@ -1,7 +0,0 @@
-// Removed: React DebugSnapshotsViewer
-// This file was intentionally cleared in cleanup commit to keep the repository focused on the Streamlit implementation.
-// If you need the original React component, check the commit history prior to this cleanup.
-
-export default function DebugSnapshotsViewerPlaceholder() {
-  return null;
-}
