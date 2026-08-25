a43effc3248221b4f1c1aed0632a7fd4270831c8
honion46 <mozberk@gmail.com>
2026-08-26 00:09:42 +0300

    Delete komponent-fiyat-bulucu-v3 directory

--- komponent-fiyat-bulucu-v3/src/adapters/base.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-from src.core.models import SearchQuery, ProductResult
-class SellerAdapter(ABC):
-    name=''
-    @abstractmethod
-    def search(self, query: SearchQuery): ...
