4b6794f4d53a302e0b7ee72a8023f6bc9b45f271
honion46 <mozberk@gmail.com>
2026-08-25 23:59:44 +0300

    Delete tests directory

--- tests/test_core.py
+++
@@ -1,8 +0,0 @@
-from src.core.query_parser import parse_query
-from src.core.normalizer import parse_price
-
-def test_query():
-    q=parse_query('LM358 DIP-8 10 adet')
-    assert q.mpn == 'LM358' and q.package == 'DIP-8' and q.quantity == 10
-
-def test_price(): assert str(parse_price('1.234,56 TL')) == '1234.56'
